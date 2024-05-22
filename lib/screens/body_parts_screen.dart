861d3655fa941bd0afa2508ad8dae656c52c945f
sara-reda-hegazy <sr717160@gmail.com>
2024-05-22 21:34:46 +0300

    add hand and face buttons

--- lib/screens/body_parts_screen.dart
+++ lib/screens/body_parts_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chappie/customWidgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class BodyPartsScreen extends StatelessWidget {
@@ -12,16 +13,58 @@ class BodyPartsScreen extends StatelessWidget {
       ),
       body:Padding(
         padding: const EdgeInsets.only(top: 20,left: 15,right: 15),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                item('Hand', 'assets/images/hand.jpg'),
+                const SizedBox(width: 15,),
+                button("Robot Hand", () { })
+              ],
+            ),
+            const SizedBox(height: 15,),
+             Row(
+              children: [
+                item("Face", 'assets/images/face.jpg'),
+                const SizedBox(width: 15,),
+                button("Robot Face", () { })
+              ],
+            ),
+          ],
+        ),
+      )
+      
+      /*Padding(
+        padding: const EdgeInsets.only(top: 20,left: 15,right: 15),
         child: GridView(
           gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,childAspectRatio:1.5,crossAxisSpacing: 15,mainAxisSpacing: 7),
            children: [
              item('Hand', 'assets/images/hand.jpg'),
              item("Face", 'assets/images/face.jpg'),
+             
            ],
            ),
       ) ,
+      */
     );
   }
+}
+
+button(String text,void Function()? onTap)
+{
+  return GestureDetector(
+    onTap: onTap,
+    child: Container(
+      alignment: Alignment.center,
+      height: 60,
+      width: 150,
+      decoration: BoxDecoration(
+        color: Colors.blueGrey,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Text(text,style:const TextStyle(color: Colors.white,fontSize: 15),),
+    ),
+  );
 }
 
 item(String text,String image)
@@ -46,8 +89,9 @@ item(String text,String image)
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Text(text,style:const TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
+                            const SizedBox(width: 10,),
                             CircleAvatar(
-                              radius: 70,
+                              radius: 40,
                               backgroundImage: AssetImage(image),
                             )
                           ],),
